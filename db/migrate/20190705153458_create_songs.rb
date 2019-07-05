42270697fc7ed437da2e6e8f24f1950e96e54e3d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-05 11:42:19 -0400

    created songs migration, model and controller

---
+++ db/migrate/20190705153458_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.text :lyrics
+      t.string :artist
+      t.string :genre
+      t.string :album
+      t.boolean :learned
+
+      t.timestamps
+    end
+  end
+end
