1035601325edc70b74142c302f6af04cd06c1f4c
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-05 11:52:27 -0400

    created element resource with migration, routes, model and controller

---
+++ db/migrate/20190705155147_create_elements.rb
@@ -0,0 +1,12 @@
+class CreateElements < ActiveRecord::Migration[5.2]
+  def change
+    create_table :elements do |t|
+      t.string :name
+      t.boolean :learned
+      t.integer :song_id
+      t.integer :instrument_id
+
+      t.timestamps
+    end
+  end
+end
