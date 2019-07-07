c7b40469da9ce492668f2a2c94f90d2a8b6d8220
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-07 12:02:35 -0400

    attempted nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :elements
   resources :songs
   resources :instruments
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :users, shallow: true do
+      resources :instruments
+      resources :songs
+      resources :elements
+    end
+   end
