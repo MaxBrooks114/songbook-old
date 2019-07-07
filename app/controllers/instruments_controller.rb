e33310a17a5d7590d1e165e3dff6372f4a3aadf7
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-07 13:14:59 -0400

    methods and logic for creating/ displaying instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,20 +1,39 @@
 class InstrumentsController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
   def index
-  end
+     @instruments = Instrument.all
+   end
 
-  def edit
-  end
+   def new
+     @instrument = Instrument.new
+   end
 
-  def destroy
-  end
+   def create
+     @instrument = Instrument.new(instrument_params)
+     if @instrument.save
+       redirect_to instrument_path(@instrument)
+     else
+       redirect_to instruments_path
+     end
+   end
+
+   def show
+     @instrument = Instrument.find(params[:id])
+   end
+
+   def edit
+     @instrument = Instrument.find(params[:id])
+   end
+
+   def update
+     instrument = Instrument.find(params[:id])
+     instrument.update(instrument_params)
+     redirect_to instrument_path(instrument)
+   end
+
+   private
+
+   def instrument_params
+     params.require(:instrument).permit(:name, :family, :range)
+   end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,7 @@
 class Instrument < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :instruments_songs
   has_many :songs, through: :instruments_songs
   has_many :elements, through: :songs
-  
+
 end
