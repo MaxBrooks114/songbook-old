ab483a6c3c8869200d92a74e0d4ad93610dde489
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-07 13:43:59 -0400

    deleted associations for development purposes

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,5 +1,5 @@
 class Element < ApplicationRecord
   belongs_to :song
   belongs_to :instrument
-  belongs_to :user 
+  # belongs_to :user
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ApplicationRecord
   has_many :instruments, through: :instruments_songs
   has_many :instruments_songs
   has_many :elements
-  belongs_to :user
+  # belongs_to :user
 end
