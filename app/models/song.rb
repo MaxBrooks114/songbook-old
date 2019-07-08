4ce52b2dcd40407060adaa524543370284901b43
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-08 19:46:18 -0400

    added song validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,5 @@ class Song < ApplicationRecord
   has_many :instruments, through: :instruments_songs
   has_many :elements
   # belongs_to :user
+  validates :title, presence: true, uniqueness: { scope: :artist }
 end
