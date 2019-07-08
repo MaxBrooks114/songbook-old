c7bfbc02de287e2ddf78e5606541a64333ef33b1
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-08 18:39:50 -0400

    built song creation methods and views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,43 @@
 class SongsController < ApplicationController
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      flash[:notice] = @song.errors.messages
+      redirect_to new_song_path
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    song = Song.find(params[:id])
+    song.update(song_params)
+    redirect_to song_path(song)
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path
+  end
+
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :lyrics, :artist, :genre, :album, :learned)
+  end
 end
