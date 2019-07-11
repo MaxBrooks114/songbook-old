2046483e6029c450c7fc8915c79a156a403f9b24
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-11 18:09:40 -0400

    built element controller forms

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,8 +1,47 @@
 class ElementsController < ApplicationController
 
-  def destroy
-    @element = Element.find(params[:id])
-    @element.destroy
-  end
+  def index
+     @elements = Element.all
+   end
 
+   def new
+     @element = Element.new
+   end
+
+   def create
+     @element = Element.new(element_params)
+     if @element.save
+       redirect_to element_path(@element)
+     else
+       flash[:notice] = @element.errors.messages
+       redirect_to new_element_path
+     end
+   end
+
+   def show
+     @element = Element.find(params[:id])
+   end
+
+   def edit
+     @element = Element.find(params[:id])
+   end
+
+   def update
+     element = Element.find(params[:id])
+     element.update(element_params)
+     redirect_to element_path(element)
+   end
+
+   def destroy
+     @element = Element.find(params[:id])
+     @element.destroy
+     redirect_to elements_path
+   end
+
+
+   private
+
+   def element_params
+     params.require(:element).permit(:name, :learned, :instrument_id, :song_id)
+   end
 end
