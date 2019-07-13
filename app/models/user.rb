56a5a4ceec2c662a32e1b977face7d19509c37d9
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-13 10:17:05 -0400

    started user signup including model validations, sign up form, and controller actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :instruments
   has_many :songs
   has_many :elements
+  has_secure_password
+  validates :email, :username, :password, :password_confirmation, presence: true
+  validates :email, :username, uniqueness: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :elements
+
   resources :songs do
     resources :elements, shallow: true
   end
