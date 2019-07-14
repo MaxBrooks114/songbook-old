5629c133fb57b56bc2f4e81f24217926f334fbe9
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-14 09:38:47 -0400

    User can sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new 
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+     if @user.save
+       session[:user_id] = @user.id
+       redirect_to user_path(@user)
+     else
+       redirect_to new_user_path 
+     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :instruments
-  has_many :songs
-  has_many :elements
+  has_many :instruments_songs
+  has_many :songs, through: :instruments_songs
+  has_many :elements, through: :songs 
   has_secure_password
   validates :email, :username, :password, :password_confirmation, presence: true
-  validates :email, :username, uniqueness: true 
+  validates :email, :username, uniqueness: true
 end
