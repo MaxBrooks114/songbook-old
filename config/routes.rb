4b940554bc9128403b1664348294b23f0e4cfc9a
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-15 17:28:48 -0400

    changed indentation on routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
 
-resources :users do
-  resources :elements
+  resources :users do
+    resources :elements
 
-  resources :songs do
-    resources :elements, shallow: true
+    resources :songs do
+      resources :elements, shallow: true
+    end
+
+
+    resources :instruments do
+      resources :songs, shallow: true
+    end
+
   end
 
-
-  resources :instruments do
-    resources :songs, shallow: true
-  end
-
-end
-
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-post '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
 
-root to: "application#home"
+  root to: "application#home"
 end
