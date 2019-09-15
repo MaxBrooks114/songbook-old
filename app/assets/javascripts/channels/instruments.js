176686a302600f20d6db9ca03143d2403b9590f7
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-15 12:23:46 -0400

    instrument form fetched with AJAX on show page and post

--- app/assets/javascripts/channels/instruments.js
+++ app/assets/javascripts/channels/instruments.js
@@ -1,42 +1,42 @@
-$(function () {
-	listenForInstrumentsClick()
-  listenForInstrumentClick()
-	listenForNewInstrumentFormClick()
+$(function() {
+  listenForInstrumentsClick()
+  listenForNewInstrumentFormClick()
 });
 
 function listenForInstrumentsClick() {
-	$('button#instruments-data').one('click', function (event) {
-		event.preventDefault()
-		getInstruments()
-	})
+  $('button#instruments-data').one('click', function(event) {
+    event.preventDefault()
+    getInstruments()
+  })
 }
+
 
 
 function getInstruments() {
-	$.ajax({
-		url: `http://localhost:3000/users/${userId}/instruments`,
-		method: 'get',
-		dataType: 'json',
-		success: function (data) {
-			console.log("the data is: ", data)
-			data.map(instrument => {
-				const newInstrument = new Instrument(instrument)
-				const newInstrumentsHtml = newInstrument.instrumentsHTML()
-				document.getElementById('ajax-instruments').innerHTML += newInstrumentsHtml
+  $.ajax({
+    url: `http://localhost:3000/users/${userId}/instruments`,
+    method: 'get',
+    dataType: 'json',
+    success: function(data) {
+      console.log("the data is: ", data)
+      data.map(instrument => {
+        const newInstrument = new Instrument(instrument)
+        const newInstrumentsHtml = newInstrument.instrumentsHTML()
+        document.getElementById('ajax-instruments').innerHTML += newInstrumentsHtml
         listenForInstrumentClick()
-        })
-      }
-	})
+      })
+    }
+  })
 
 }
+
 
 
 
 function listenForInstrumentClick() {
-	$("button.instrument-data").one('click', function (event) {
-    debugger
+  $("button.instrument-data").one('click', function(event) {
     let id = $(this).attr('data-id')
-		event.preventDefault()
+    event.preventDefault()
     console.log('click registered')
     fetch(`/users/${userId}/instruments/${id}.json`)
       .then(res => res.json())
@@ -44,8 +44,8 @@ function listenForInstrumentClick() {
         const newInstrument = new Instrument(instrument)
         const newInstrumentHtml = newInstrument.instrumentHTML()
         document.getElementById(`instrument-${id}-details`).innerHTML += newInstrumentHtml
+      })
   })
-	})
 }
 
 
@@ -53,54 +53,52 @@ function listenForInstrumentClick() {
 
 
 function listenForNewInstrumentFormClick() {
-	$('button#ajax-new-instrument').on('click', function (event) {
-		event.preventDefault()
-		let newInstrumentForm = Instrument.newInstrumentForm()
-		// $('div#new-instrument-form-div')
-		document.querySelector('div#new-instrument-form-div').innerHTML = newInstrumentForm
-	})
+  $('button#ajax-new-instrument').on('click', function(event) {
+    event.preventDefault()
+    $.ajax({
+      url: `http://localhost:3000/users/${userId}/instruments/new`,
+      method: 'get',
+      dataType: 'html',
+    }).success(function(response) {
+      document.getElementById("new-instrument-form-div").innerHTML += response
+      postInstrument()
+    })
+  })
+
 }
+
+
+function postInstrument() {
+  $("form").submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: `http://localhost:3000/users/${userId}/instruments`,
+      data: $(this).serialize(),
+      dataType: "json",
+    })
+  })
+}
+
 
 class Instrument {
-	constructor(obj) {
-		this.id = obj.id
-		this.i_name = obj.i_name
-    this.make = obj.make
-    this.model = obj.model
-    this.range = obj.range
-    this.family = obj.family
-		this.elements = obj.elements
-		this.songs = obj.songs
-    this.display_name = obj.display_name
-	}
+  constructor(instrument) {
+    this.id = instrument.id
+    this.i_name = instrument.i_name
+    this.make = instrument.make
+    this.model = instrument.model
+    this.range = instrument.range
+    this.family = instrument.family
+    this.elements = instrument.elements
+    this.songs = instrument.songs
+    this.display_name = instrument.display_name
+  }
 
-	static newInstrumentForm() {
-		return (`
-		<strong>New instrument form</strong>
-			<form action = "/users/${userId}/instruments" method= "post">
-        <input type='text' name='instrument-i_name' placeholder="Instrument"></input><br>
-        <input type='text' name='instrument-make' placeholder="Make"></input><br>
-        <input type='text' name='instrument-make' placeholder="Model"></input><br>
-				<p> Family: <select id='instrument-family' name='instrument-family' placeholder="family">
-            <option value="Brass">Brass</option>
-            <option value="String">String</option>
-            <option value="Woodwind">Woodwind</option>
-            <option value="Percussion">Percussion</option>
-            <option value="Other">Other</option>
-        </select></p>
-        <p> Range: <select id='instrument-range' name='instrument-i_name' placeholder="range">
-            <option value ="Soprano">Soprano</option>
-            <option value="Alto">Alto</option>
-            <option value="Tenor">Tenor</option>
-            <option value="Baritone">Baritone</option>
-            <option value= "N/A">NA</option>
-        </select></p>
-				<input type='submit' />
-			</form>
-		`)
-	}
+
+
 }
-Instrument.prototype.instrumentsHTML = function () {
+
+Instrument.prototype.instrumentsHTML = function() {
   return (`
     <div class="instrument" data-id= "${this.id}">
       <p>${this.display_name}</p>
@@ -108,22 +106,22 @@ Instrument.prototype.instrumentsHTML = function () {
       <button data-id= "${this.id}" class='instrument-data'> See more </button>
     </div>
     `)
-  }
+}
 
 
 
-Instrument.prototype.instrumentHTML = function () {
-	let instrumentSongs = this.songs.map(song => {
-		return (`
+Instrument.prototype.instrumentHTML = function() {
+  let instrumentSongs = this.songs.map(song => {
+    return (`
 			<li>${song.title}</li>
 		`)
-	}).join('')
+  }).join('')
   let instrumentElements = this.elements.map(element => {
     return (`
       <li>${element.full_name}</li>
     `)
   }).join('')
-	return (`
+  return (`
 
 			<p> Range: ${this.range}</p>
       <p> Family: ${this.family}</p>
--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,75 +1,74 @@
+# frozen_string_literal: true
+
 class InstrumentsController < ApplicationController
-   before_action :require_login, :set_user
-   skip_before_action :verify_authenticity_token
-   def index
-     if params[:family]
-        @instrument = @user.instruments.find_by(family: params[:family])
-     end
-     @instruments = @user.instruments.filter_by(params.slice(:range, :family, :make))
-     respond_to do |f|
-       f.html {render :index}
-       f.json {render json: @instruments}
+  before_action :require_login, :set_user
+
+  def index
+    if params[:family]
+      @instrument = @user.instruments.find_by(family: params[:family])
+    end
+    @instruments = @user.instruments.filter_by(params.slice(:range, :family, :make))
+    respond_to do |f|
+      f.html { render :index }
+      f.json { render json: @instruments }
+    end
+ end
+
+  def new
+    @instrument = Instrument.new
+    respond_to do |f|
+      f.html { render :new, layout: false }
     end
   end
 
-   def new
-     @instrument = Instrument.new
-   end
-
-   def create
-     @instrument = Instrument.new(instrument_params)
-     if @instrument.save
-       # redirect_to user_instrument_path(@user, @instrument)
-       respond_to do |f|
-          f.html {redirect_to user_instrument_path(@user, @instrument)}
-
-          f.json {render json: @instrument.to_json(include: [:songs, :elements])}
-        end
-     else
-       render 'new'
-     end
-   end
-
-   def show
-     set_instrument
-     respond_to do |f|
-        f.html {render :show}
-
-        f.json {render json: @instrument}
-
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      respond_to do |f|
+        f.html { redirect_to user_instrument_path(@user, @instrument) }
+        f.json { render json: @instrument }
+      end
+    else
+      render 'new'
     end
-   end
+  end
 
-   def edit
-     set_instrument
-   end
+  def show
+    set_instrument
+    respond_to do |f|
+      f.html { render :show }
 
-   def update
-     set_instrument
-     if @instrument.update(instrument_params)
-       redirect_to user_instrument_path(@user, @instrument)
-     else
+      f.json { render json: @instrument }
+    end
+  end
+
+  def edit
+    set_instrument
+  end
+
+  def update
+    set_instrument
+    if @instrument.update(instrument_params)
+      redirect_to user_instrument_path(@user, @instrument)
+    else
       render 'edit'
-   end
- end
+  end
+end
 
-   def destroy
-     set_instrument
-     @instrument.destroy
-     redirect_to user_instruments_path(@user)
-   end
+  def destroy
+    set_instrument
+    @instrument.destroy
+    redirect_to user_instruments_path(@user)
+  end
 
+  private
 
-   private
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+    redirect_to user_instruments_path(@user) unless @instrument
+  end
 
-   def set_instrument
-     @instrument = Instrument.find(params[:id])
-     if !@instrument
-       redirect_to user_instruments_path(@user)
-     end
-   end
-
-   def instrument_params
-     params.require(:instrument).permit(:i_name, :make, :model, :picture, :delete_picture, :family, :range, :user_id)
-   end
+  def instrument_params
+    params.require(:instrument).permit(:i_name, :make, :model, :picture, :delete_picture, :family, :range, :user_id)
+  end
 end
