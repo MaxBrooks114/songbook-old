81e7d6f11431da16cf754e9af4b46f89ee068994
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-16 18:27:18 -0400

    changed getSongs function to have only title and artist

--- app/assets/javascripts/channels/songs.js
+++ app/assets/javascripts/channels/songs.js
@@ -1,54 +1,56 @@
-$(function () {
-	listenForSongsClick()
-	listenForNewSongFormClick()
+$(function() {
+  listenForSongsClick()
+  listenForNewSongFormClick()
 });
 
 function listenForSongsClick() {
-	$('button#songs-data').one('click', function (event) {
-		event.preventDefault()
-		getSongs()
-	})
+  $('button#songs-data').one('click', function(event) {
+    event.preventDefault()
+    getSongs()
+  })
 }
 
+
 function getSongs() {
-	$.ajax({
-		url: `http://localhost:3000/users/${userId}/songs`,
-		method: 'get',
-		dataType: 'json',
-		success: function (data) {
-			console.log("the data is: ", data)
-			data.map(song => {
-				const newSong = new Song(song)
-				const newSongHtml = newSong.songHTML()
-				document.getElementById('ajax-songs').innerHTML += newSongHtml
-			})
-		}
-	})
+  $.ajax({
+    url: `http://localhost:3000/users/${userId}/songs`,
+    method: 'get',
+    dataType: 'json',
+    success: function(data) {
+      console.log("the data is: ", data)
+
+      data.map(song => {
+        const newSong = new Song(song)
+        const newSongsHtml = newSong.songsHTML()
+        document.getElementById('ajax-songs').innerHTML += newSongsHtml
+      })
+    }
+  })
 }
 
 function listenForNewSongFormClick() {
-	$('button#ajax-new-song').on('click', function (event) {
-		event.preventDefault()
-		let newSongForm = Song.newSongForm()
-		// $('div#new-song-form-div')
-		document.querySelector('div#new-song-form-div').innerHTML = newSongForm
-	})
+  $('button#ajax-new-song').on('click', function(event) {
+    event.preventDefault()
+    let newSongForm = Song.newSongForm()
+    // $('div#new-song-form-div')
+    document.querySelector('div#new-song-form-div').innerHTML = newSongForm
+  })
 }
 
 class Song {
-	constructor(obj) {
-		this.id = obj.id
-		this.title= obj.title
-    this.artist = obj.artist
-    this.genre = obj.genre
-    this.album= obj.lyrics
-    this.lyrics = obj.lyrics
-		this.elements = obj.elements
-		this.instruments = obj.instruments
-	}
+  constructor(song) {
+    this.id = song.id
+    this.title = song.title
+    this.artist = song.artist
+    this.genre = song.genre
+    this.album = song.lyrics
+    this.lyrics = song.lyrics
+    this.elements = song.elements
+    this.instruments = song.instruments
+  }
 
-	static newSongForm() {
-		return (`
+  static newSongForm() {
+    return (`
 		<strong>New song comment form</strong>
 			<form>
 				<input id='song-title' type='text' name='title'></input><br>
@@ -56,21 +58,31 @@ class Song {
 				<input type='submit' />
 			</form>
 		`)
-	}
+  }
 }
 
-Song.prototype.songHTML = function () {
-	let songInstruments = this.instruments.map(instrument => {
-		return (`
+Song.prototype.songsHTML = function() {
+  return (`
+     <div class="song" data-id= "${this.id}">
+          ${this.title} (${this.artist})
+        </div
+    `
+
+  )
+}
+
+Song.prototype.songHTML = function() {
+  let songInstruments = this.instruments.map(instrument => {
+    return (`
 			<p>${instrument.display_name}</p>
 		`)
-	}).join('')
+  }).join('')
   let songElements = this.elements.map(element => {
     return (`
       <p>${element.full_name}</p>
     `)
   }).join('')
-	return (`
+  return (`
 		<div class='song'>
 			<p>${this.title} Instruments: ${songInstruments} Elements: ${songElements} <p>
 
