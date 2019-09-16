8bd152747e11986fc26d0ed89b674f85f6139edc
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-16 19:49:48 -0400

    changed elements index display text and added "see more" button

--- app/assets/javascripts/channels/elements.js
+++ app/assets/javascripts/channels/elements.js
@@ -1,70 +1,62 @@
-$(function () {
-	listenForElementsClick()
-	listenForNewElementFormClick()
+$(function() {
+  listenForElementsClick()
+  listenForNewElementFormClick()
 });
 
 function listenForElementsClick() {
-	$('button#elements-data').one('click', function (event) {
-		event.preventDefault()
-		getElements()
-	})
+  $('button#elements-data').one('click', function(event) {
+    event.preventDefault()
+    getElements()
+  })
 }
 
 function getElements() {
-	$.ajax({
-		url: `http://localhost:3000/users/${userId}/elements`,
-		method: 'get',
-		dataType: 'json',
-		success: function (data) {
-			console.log("the data is: ", data)
-			data.map(element => {
-				const newElement = new Element(element)
-				const newElementHtml = newElement.elementHTML()
-				document.getElementById('ajax-elements').innerHTML += newElementHtml
-			})
-		}
-	})
+  $.ajax({
+    url: `http://localhost:3000/users/${userId}/elements`,
+    method: 'get',
+    dataType: 'json',
+    success: function(data) {
+      console.log("the data is: ", data)
+      data.map(element => {
+        const newElement = new Element(element)
+        const newElementsHtml = newElement.elementsHTML()
+        document.getElementById('ajax-elements').innerHTML += newElementsHtml
+      })
+    }
+  })
 }
 
 function listenForNewElementFormClick() {
-	$('button#ajax-new-element').on('click', function (event) {
-		event.preventDefault()
-		let newElementForm = Element.newElementForm()
-		// $('div#new-element-form-div')
-		document.querySelector('div#new-element-form-div').innerHTML = newElementForm
-	})
+  $('button#ajax-new-element').on('click', function(event) {
+    event.preventDefault()
+    let newElementForm = Element.newElementForm()
+    // $('div#new-element-form-div')
+    document.querySelector('div#new-element-form-div').innerHTML = newElementForm
+  })
 }
 
 class Element {
-	constructor(obj) {
-		this.id = obj.id
-		this.e_name = obj.e_name
-    this.tempo = obj.tempo
-    this.key = obj.key
-		this.lyrics= obj.lyrics
-		this.learned = obj.learned
-    this.full_name = obj.full_name
-    this.songs = obj.songs
-    this.instruments = obj.instruments
-	}
+  constructor(element) {
+    this.id = element.id
+    this.e_name = element.e_name
+    this.tempo = element.tempo
+    this.key = element.key
+    this.lyrics = element.lyrics
+    this.learned = element.learned
+    this.full_name = element.full_name
+    this.songs = element.songs
+    this.instruments = element.instruments
+  }
 
-	static newElementForm() {
-		return (`
-		<strong>New element comment form</strong>
-			<form>
-				<input id='element-title' type='text' name='title'></input><br>
-				<input type='text' name='content'></input><br>
-				<input type='submit' />
-			</form>
-		`)
-	}
+
 }
 
-Element.prototype.elementHTML = function () {
-	return (`
-		<div class='element'>
-			<p>${this.full_name} <p>
-
-		</div>
-	`)
+Element.prototype.elementsHTML = function() {
+  return (`
+    <div class="element" data-id= "${this.id}">
+      <p>${this.full_name}</p>
+      <div id= element-${this.id}-details> </div>
+      <button data-id= "${this.id}" class='element-data'> See more </button>
+    </div>
+    `)
 }
