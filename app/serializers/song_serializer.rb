079064a00971b41ed8fe33c4313bd21dc509c7dd
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-16 18:26:56 -0400

    added artist attribute to song serializer model

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :title, :lyrics, :genre, :album
+  attributes :id, :title, :lyrics, :genre, :album, :artist
 
   has_many :instruments
   has_many :elements
