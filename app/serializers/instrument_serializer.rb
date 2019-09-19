27abfaec09c0a80d7b443c1110701034a46f6324
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-19 18:41:09 -0400

    added route helpers to serialize active storage images

--- app/serializers/instrument_serializer.rb
+++ app/serializers/instrument_serializer.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class InstrumentSerializer < ActiveModel::Serializer
-  attributes :id, :i_name, :family, :range, :make, :model, :display_name
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :i_name, :family, :range, :make, :model, :display_name, :picture
 
   has_many :songs
   has_many :elements
+
+  def picture
+    rails_blob_path(object.picture, disposition: 'attachment', only_path: true) if object.picture.attached?
+   end
 end
