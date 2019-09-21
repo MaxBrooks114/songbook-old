cf292440dc3e417a80f805a40857319c617643d8
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-21 09:41:42 -0400

    added methods for activestorage blob url display

--- app/serializers/element_serializer.rb
+++ app/serializers/element_serializer.rb
@@ -1,6 +1,22 @@
+# frozen_string_literal: true
+
 class ElementSerializer < ActiveModel::Serializer
-  attributes :id, :e_name, :tempo, :key, :lyrics, :learned, :full_name
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :e_name, :tempo, :key, :lyrics, :learned, :full_name, :sheet_music, :recording
 
   belongs_to :song
   belongs_to :instrument
+
+  def sheet_music
+    rails_blob_path(object.sheet_music, disposition: 'attachment', only_path: true) if object.sheet_music.attached?
+  end
+
+  def recording
+    rails_blob_path(object.recording, only_path: true) if object.recording.attached?
+  end
+
+  def tempo
+    object.tempo.nil? ? '' : object.tempo
+  end
 end
