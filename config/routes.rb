076567b35a716df9d023037e8187d642cb5b74c0
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-22 02:40:34 -0400

    added stats route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   resources :users do
+    get 'stats', to: 'users#stats'
     resources :elements
 
     resources :songs do
       resources :elements, shallow: true
     end
 
-
     resources :instruments do
       resources :songs, shallow: true
     end
-
   end
 
   get 'login', to: 'sessions#new'
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: 'sessions#google_create'
   get 'auth/failure', to: 'application#home'
 
-  root to: "application#home"
+  root to: 'application#home'
 end
