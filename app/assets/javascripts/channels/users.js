41a8b108b882fbb57f3d4d183d839667e5fd0160
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-09-22 02:40:27 -0400

    added stats ajax function

--- app/assets/javascripts/channels/users.js
+++ app/assets/javascripts/channels/users.js
@@ -1,1 +1,18 @@
+$(function() {
+  getStatsOnClick()
+});
 
+function getStatsOnClick() {
+  $('button#stats-data').on('click', function(event) {
+    event.preventDefault()
+    $.ajax({
+      url: `http://localhost:3000/users/${userId}/stats`,
+      method: 'get',
+      dataType: 'html',
+    }).success(function(response) {
+      clearContainer()
+      document.getElementById("ajax-stats").innerHTML = response
+    })
+  })
+
+}
